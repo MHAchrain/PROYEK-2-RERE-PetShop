0af4731c037e796e7bfdda930b0f94e0b9906669
sakhiard <ardrasakhi390@gmail.com>
2026-05-21 20:54:39 +0700

    FLUTTER

--- app_rere_petshop/lib/core/constants/app_constants.dart
+++ app_rere_petshop/lib/core/constants/app_constants.dart
@@ -2,10 +2,7 @@
 
 class AppConstants {
   // Ganti dengan URL Laravel kamu
-  static const String baseUrl =
-      'https://api.rerepetshop.biz.id/api'; // untuk Android emulator
-  // static const String baseUrl = 'http://localhost:8000/api'; // untuk Chrome/web
-  // static const String baseUrl = 'https://your-domain.com/api'; // untuk production
+  static const String baseUrl = 'https://api.rerepetshop.biz.id/api';
 
   static const String appName = 'ReRe Petshop';
   static const String whatsappNumber = '6281319410250'; // 0813-1941-0250
