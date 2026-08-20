997e9ea2bd43a228e862ad7c2f3abc50cf67f7ed
sakhiard <ardrasakhi390@gmail.com>
2026-08-21 01:52:34 +0700

    bismillahirahmanirahhim 8

--- backend-laravel/config/database.php
+++ backend-laravel/config/database.php
@@ -41,13 +41,13 @@ return [
             'journal_mode' => null,
             'synchronous' => null,
         ],
-       'mysql' => [
+      'mysql' => [
     'driver' => 'mysql',
     'url' => env('DB_URL'),
     'host' => env('DB_HOST', '127.0.0.1'),
     'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'forge'),
-    'username' => env('DB_USERNAME', 'forge'),
+    'database' => env('DB_DATABASE', 'laravel'),
+    'username' => env('DB_USERNAME', 'root'),
     'password' => env('DB_PASSWORD', ''),
     'unix_socket' => env('DB_SOCKET', ''),
     'charset' => env('DB_CHARSET', 'utf8mb4'),
@@ -57,8 +57,9 @@ return [
     'strict' => true,
     'engine' => null,
     'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') && file_exists(env('MYSQL_ATTR_SSL_CA')) ? env('MYSQL_ATTR_SSL_CA') : null,
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', file_exists('/etc/pki/tls/certs/ca-bundle.crt') 
+            ? '/etc/pki/tls/certs/ca-bundle.crt' 
+            : (file_exists('/etc/ssl/certs/ca-certificates.crt') ? '/etc/ssl/certs/ca-certificates.crt' : null)),
     ]) : [],
 ],
         'mariadb' => [
