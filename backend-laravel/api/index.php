4df446e1c390f4ba01435c095e2b71ffd7b5b9ac
sakhiard <ardrasakhi390@gmail.com>
2026-08-20 23:22:49 +0700

    nambah

--- backend-laravel/api/index.php
+++ backend-laravel/api/index.php
@@ -1,3 +1,19 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Pastikan storage dan cache menggunakan /tmp
+$app->useStoragePath('/tmp/storage');
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
